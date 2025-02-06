6e8f385a53d8b965971ee9a4e2224833271e3899
camilaflores07 <camila_maria@unitec.edu>
2025-02-06 15:58:28 -0600

    Hola mundo!

--- src/wiwiwi/Wiwiwi.java
+++ src/wiwiwi/Wiwiwi.java
@@ -4,6 +4,11 @@ public class Wiwiwi {
 
     public static void main(String[] args) {
         System.out.println("sopitaconpapas");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" Hola");
+        }
+        
     }
     
 }
